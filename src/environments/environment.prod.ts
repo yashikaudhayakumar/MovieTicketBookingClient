bdc052e71959005db3d48e13ff15111820678c29
charan9840 <charana598@gmail.com>
2023-07-27 19:11:53 +0530

    server api url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://movie-booking-clientapp.azurewebsites.net/api/v1',
+  apiBaseUrl: 'https://movie-ticket-backend.azurewebsites.net/api/v1',
 };
